725a2d529cccf0d3b62e454d56f1da10a9d627b6
jadenlimjc <116095460+jadenlimjc@users.noreply.github.com>
2024-02-22 23:02:46 +0800

    fixed sidebar issue, changed logos

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,21 +1,25 @@
 import './index.scss'
 import {Link, NavLink} from 'react-router-dom'
-import LogoS from '../../assets/images/logo-s.png'
+import LogoJ from '../../assets/images/logo-j.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUser, faEnvelope, faGlobe } from '@fortawesome/free-solid-svg-icons'; // Import specific FontAwesome icons
+import { faHome, faUser, faEnvelope,} from '@fortawesome/free-solid-svg-icons'; // Import specific FontAwesome icons
+import {
+    faLinkedin,
+    faGithub,
+  } from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
-<div className = 'nav-bar'>
-    <Link className = 'logo' to='/'>
-        <img src={LogoS} alt="logo" />
-        <img className= "sub-logo" src={LogoSubtitle} alt="Jaden" />
+<div className = "nav-bar">
+    <Link className = "logo" to="/">
+        <img src={LogoJ} alt="logo" />
+        <img className= "sub-logo" src={LogoSubtitle} alt="wave" />
     </Link>
     <nav>
         <NavLink exact = "true" activeclassname="active" to = "/">
             <FontAwesomeIcon icon = {faHome} color= "#4d4d4e" />
         </NavLink>
-        <NavLink exact = "true" activeclassname="active" className = "about-Link" to = "/about">
+        <NavLink exact = "true" activeclassname="active" className = "about-link" to = "/about">
             <FontAwesomeIcon icon = {faUser} color= "#4d4d4e" />
         </NavLink>
         <NavLink exact = "true" activeclassname="active" className = "contact-link" to = "/contact">
@@ -29,7 +33,16 @@ const Sidebar = () => (
               rel="noreferrer"
               href="https://www.linkedin.com/in/jadenlimjc/"
             >
-                <FontAwesomeIcon icon={faGlobe} color= "#4d4d4e" />
+                <FontAwesomeIcon icon={faLinkedin} color= "#4d4d4e" />
+            </a>
+        </li>
+        <li>
+            <a 
+              target="_blank" 
+              rel="noreferrer"
+              href="https://github.com/jadenlimjc"
+            >
+                <FontAwesomeIcon icon={faGithub} color= "#4d4d4e" />
             </a>
         </li>
     </ul>
